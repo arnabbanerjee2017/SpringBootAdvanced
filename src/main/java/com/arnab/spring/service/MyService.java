f818d6215b19a42f9c435c4b6b8965317a4b513c
Arnab Banerjee <arnab.ban09@gmail.com>
2019-07-20 01:21:42 +0530

    Changes related to devtools

---
+++ src/main/java/com/arnab/spring/service/MyService.java
@@ -0,0 +1,12 @@
+package com.arnab.spring.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+
+	public String getValue() {
+		return "Hello friends! How are you? I am fine. Thankyou!";
+	}
+	
+}
